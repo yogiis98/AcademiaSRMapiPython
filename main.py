2ceb1d97209d0a8756b53e14343f8e2b4f027b59
yogiis98 <yogeshyadavme@gmail.com>
2018-03-25 16:36:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,4 +82,5 @@ def getPersonalDetails():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    from os import environ
+    app.run(debug=False, host='0.0.0.0', port=environ.get("PORT", 5000))
